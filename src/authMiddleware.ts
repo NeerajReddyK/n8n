beb17676d009572241054a387acf2cd614b78ef3
Neeraj Reddy <neerajreddy0059@gmail.com>
2025-09-12 12:34:49 +0530

    completed authMiddleware logic

--- src/authMiddleware.ts
+++ src/authMiddleware.ts
@@ -1,17 +1,37 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
-export const authMiddleware = (
+export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const auth = localStorage.getItem("token");
-    if (!auth) {
-      res.status(401).json({ message: "token is not provided" });
+    const token = req.headers.authorization;
+    if (!token) {
+      res.status(404).json({
+        message: "token is undefined",
+      });
       return;
     }
-    const token = auth.split(" ")[1];
+    const jwtToken = token.split(" ")[1];
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      res.status(500).json({
+        message: "undefined jwtSecret",
+      });
+      return;
+    }
+    const decoded = jwt.verify(jwtToken!, secret);
+    if (!decoded) {
+      res.status(400).json({
+        message: "Invalid user",
+      });
+      return;
+    }
+    req.email = decoded as string;
+
+    next();
   } catch (error) {
     console.error("Error validating token: ", error);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import signRouter from "./routes/sign.js";
+import workflowRouter from "./routes/workflow.js";
 
 const app = express();
 app.use(express.json());
 app.use("/api/v1", signRouter);
+app.use("/api/v1/workflow", workflowRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
