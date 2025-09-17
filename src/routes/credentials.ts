fb0474e912fd7cf5438462a20a79865f77c53065
Neeraj Reddy <neerajreddy0059@gmail.com>
2025-09-17 16:41:51 +0530

    added telegram token validation

--- src/routes/credentials.ts
+++ src/routes/credentials.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../authMiddleware.js";
 import { prismaClient } from "../db/prismaClient.js";
+import axios from "axios";
 
 const router = express.Router();
 
@@ -51,6 +52,53 @@ router.delete("/:id", authMiddleware, async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "unable to delete credential at the moment",
+    });
+  }
+});
+
+router.post("/telegram", authMiddleware, async (req, res) => {
+  const email = req.email;
+  try {
+    const { apiToken, workflowId } = req.body;
+    if (!apiToken || !workflowId) {
+      res
+        .status(404)
+        .json({ message: "api token and workflowId are required" });
+      return;
+    }
+    const baseUrl = "https://api.telegram.org/";
+    const user = await axios.post(`${baseUrl}bot${apiToken}/getMe`);
+    if (!user.data.ok) {
+      res.status(404).json({
+        message: "Invalid api token",
+      });
+      return;
+    }
+
+    // store to db
+    const db = await prismaClient.credentials.create({
+      data: {
+        email,
+        title: "Telegram Credentials",
+        platform: "telegram",
+        data: {
+          apiToken: apiToken,
+        },
+      },
+    });
+
+    if (!db) {
+      res.status(500).json({
+        message: "Unable to write credentials to db",
+      });
+      return;
+    }
+
+    res.status(200).json({ success: "success", result: db });
+  } catch (error) {
+    res.status(500).json({
+      message: "fail, unable to validate token at the moment",
+      error,
     });
   }
 });
