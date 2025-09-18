def224c0a2fcac305e0818863a9d08f5f055f93f
Neeraj Reddy <neerajreddy0059@gmail.com>
2025-09-18 22:22:52 +0530

    fixed for loop

--- src/nodes/runWorkflow.ts
+++ src/nodes/runWorkflow.ts
@@ -3,8 +3,8 @@ import runNode from "./runNode.js";
 export const runWorkflow = async (workflow: any, inputData: any) => {
   const { nodes } = workflow;
   let data = inputData;
-  for (const node in nodes) {
+  for (const node of nodes) {
     data = await runNode(node, data);
-    console.log("executing node: ", node);
+    console.log("executing node: ", node.type);
   }
 };
